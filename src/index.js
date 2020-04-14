1baf24e24faf5a91125c325493265f49bcb15e8c
Tom Coemans <tom.coemans@gmail.com>
2020-04-14 10:56:57 +0200

    setup

---
+++ src/index.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    VlBody: require('../dist/vl-body.src.js').VlBody
+}
---
+++ test/e2e/body.test.js
@@ -0,0 +1,16 @@
+const { assert, driver, By } = require('vl-ui-core').Test.Setup;
+const VlBodyPage = require('./pages/vl-body.page');
+
+describe('vl-body', async () => {
+    const vlBodyPage = new VlBodyPage(driver);
+
+    before(() => {
+        return vlBodyPage.load();
+    });
+
+    it('als gebruiker kan ik de inhoud van een body element zien', async () => {
+        const body = await vlBodyPage.getBody();
+        const p = await body.findElement(By.css('p'));
+        await assert.eventually.equal(p.getText(), 'Door gebruik te maken van het native vl-body element incl. stijl zal er een minimum aan huisstijl geladen worden.');
+    });
+});
